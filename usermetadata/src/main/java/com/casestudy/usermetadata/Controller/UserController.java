fc2665a38b0810899bbafe356f27fd4c3030a5df
Ali Yar Khan <aliyark145@gmail.com>
2022-05-07 21:20:57 +0500

    Logger added

--- usermetadata/src/main/java/com/casestudy/usermetadata/Controller/UserController.java
+++ usermetadata/src/main/java/com/casestudy/usermetadata/Controller/UserController.java
@@ -1,6 +1,8 @@
 //handle all request from the frontend
 package com.casestudy.usermetadata.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,8 @@ import com.casestudy.usermetadata.Services.UserService;
 @RequestMapping("/offerzone/users/")
 public class UserController {
 	
+	Logger logger = LoggerFactory.getLogger(UserController.class);
+	
 	@Autowired
 	UserService userService;
 	
@@ -28,21 +32,23 @@ public class UserController {
 //  postEndPoint we can take data from frontend and from post mechanism we can save data in backend server 
 	@PostMapping("saveUserDetails")
 	public UserMetaData saveUserDetail(@RequestBody UserMetaData userData) {
+		logger.trace("Saving new user");
 		UserMetaData user= userService.saveUserMetaDataTODB(userData);
 		return user;
 	}
 	
 	@PutMapping("/search/{userId}")
 	public UserMetaData updateUser() {
+		logger.trace("Updating user");
 		return userService.getUserDetailsFromDB(toString());		 
 	}
 	
 //  getEndPoint is used to fetch data from backend to frontend
     @GetMapping("getUserDetails/{name}")
 	public UserMetaData getUserDetails(@PathVariable String name) {
-		UserMetaData user=userService.getUserDetailsFromDB(name);
+    	logger.trace("Getting user");
+    	UserMetaData user=userService.getUserDetailsFromDB(name);
 		return user;
 	}
-	
 
 }
